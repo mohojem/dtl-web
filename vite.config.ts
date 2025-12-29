f702d625d5abc2a40c8c5d14732256f9e6ce1a37
MPHO MOHOJE <mohoje2mpho@gmail.com>
2025-12-29 17:19:25 +0200

    Fix base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/dtl-elevated-web/",
+  base: "/dtl-elevated-web//",
   server: {
     host: "::",
     port: 8080,
